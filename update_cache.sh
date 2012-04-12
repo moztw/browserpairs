46e9a7623a7ba8f75bfc72c69b1a67306232dbbc
Timothy Guan-tin Chien <timdream@gmail.com>
2012-04-12 13:52:27 +0200

    Real fix for Linux, add desc

--- update_cache.sh
+++ update_cache.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
+#
+# This script is design to run before every commit to update the hash
+# within site.appcache
+#
+# You would still need to manually maintain the list of files.
+# This script only update the hash of all files
+#
+
+# FreeBSD & Darwin comes with md5, but on Linux we need to use md5sum
 [ ! -z `which md5` ] && MD5=md5
 [ ! -z `which md5sum` ] && MD5=md5sum
 
 if [ -z $MD5 ]; then
   echo 'Error: md5 tool not found'
   exit;
+fi
+
+# Linux use different version of sed than others
+# which has problem interpreting the empty extension of -i parameter.
+if [ `uname -s` = Linux ]; then
+  SED='sed -i'
+else
+  SED="sed -i ''"
 fi
 
 # Download Remote files to include them into hash calculation
@@ -16,6 +33,6 @@ mkdir -p .tmp
 # Update hash value in cache.manifest by generate md5 hash of all local files
 
 HASH=`find . -type f | grep -v \.git | grep -v "site\.appcache" | xargs cat - | $MD5`
-eval "sed -i '' -e 's/^# hash.*$/# hash "$HASH"/' site.appcache"
+eval $SED" -e 's/^# hash.*$/# hash "$HASH"/' site.appcache"
 
 rm -R .tmp
