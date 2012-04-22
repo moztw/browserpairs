5c468e3efa51b8ca840dcf0e4f3814350c2b324a
Timothy Guan-tin Chien <timdream@gmail.com>
2012-04-23 00:04:55 +0800

    Server-side update_cache.sh

--- update_cache.sh
+++ update_cache.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
 
 #
-# This script is design to run before every commit to update the hash
+# This script is design to run after every pull to update the hash
 # within site.appcache
 #
 # You would still need to manually maintain the list of files.
 # This script only update the hash of all files
 #
-
-# FreeBSD & Darwin comes with md5, but on Linux we need to use md5sum
-[ ! -z `which md5` ] && MD5=md5
-[ ! -z `which md5sum` ] && MD5=md5sum
-
-if [ -z $MD5 ]; then
-  echo 'Error: md5 tool not found'
-  exit;
-fi
 
 # Linux use different version of sed than others
 # which has problem interpreting the empty extension of -i parameter.
@@ -25,14 +16,5 @@ else
   SED="sed -i ''"
 fi
 
-# Download Remote files to include them into hash calculation
-mkdir -p .tmp
-#wget -q -O .tmp/webfont 'http://fonts.googleapis.com/css?family=Gloria+Hallelujah|Merriweather:700'
-#wget -q -O .tmp/gravatar 'http://gravatar.com/avatar/2becaf1073957bdad2f06e183731131d?s=200'
-
-# Update hash value in cache.manifest by generate md5 hash of all local files
-
-HASH=`find . -type f | grep -v \.git | grep -v "site\.appcache" | xargs cat - | $MD5`
+HASH=`git rev-parse HEAD`
 eval $SED" -e 's/^# hash.*$/# hash "$HASH"/' site.appcache"
-
-rm -R .tmp
